b0fda979943ab84c19fe91d46c9f66b69933aac4
Juan Yepes Parra <juan@MacBook-Pro-de-Juan.local>
2021-01-28 23:57:41 -0500

    Laboratorio 1

--- App/controller.py
+++ App/controller.py
@@ -54,6 +54,6 @@ def loadTags(filename):
 
 def loadBooksTags(catalog):
     """
-    TODO
+    Carga todos los tags de los libros
     """
     pass
--- App/view.py
+++ App/view.py
@@ -42,7 +42,7 @@ def printMenu():
     print("Opciones:")
     print("1- Cargar Libros")
     print("2- Cargar Tags")
-    # TO-DO: Modificaciones para completar el laboratorio 1.
+    print("3- Cargar Tags de los Libros")
     print("0- Salir")
 
 
@@ -59,6 +59,14 @@ def loadTags():
     """
     return controller.loadTags('GoodReads/tags.csv')
 
+
+def loadBooksTags():
+    """
+    Carga los tags de los libros
+    """
+    return controller.loadBooksTags('GoodReads/book_tags-small.csv')
+
+
 """
 Menu principal
 """
@@ -74,8 +82,11 @@ while True:
         print("Cargando información de tags....")
         tags = loadTags()
         print('Total de tags cargados: ' + str(lt.size(tags)))
-    
-    # TO-DO: Modificaciones para completar el laboratorio 1.
+
+    elif int(inputs[0]) == 3:
+        print("Cargando los tags de los libros....")
+        books_tags = loadBooksTags()
+        print('Total de tags cargados: ' + str(lt.size(books_tags)))
 
     else:
         sys.exit(0)
